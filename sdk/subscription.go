c856cbb2785d4951ddbbdb6354dcf95acd85a220
Oliviu Sivu <os@visitor-analytics.io>
2024-07-02 10:52:12 +0300

    add deactivate method to subscriptions api

--- sdk/subscription.go
+++ sdk/subscription.go
@@ -126,6 +126,37 @@ func (t *TwiplaSubscriptionAPI) Resume(websiteExtID string) error {
 	return nil
 }
 
+func (t *TwiplaSubscriptionAPI) Deactivate(websiteExtID string) error {
+	jsonData, err := json.Marshal(struct {
+		IntpWebsiteId string `json:"intpWebsiteId"`
+	}{
+		IntpWebsiteId: websiteExtID,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	url := t.client.apiGateway + "/v2/3as/notifications/subscriptions/deactivate"
+	r, err := t.client.NewRequest(http.MethodPost, url, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return err
+	}
+
+	res, err := http.DefaultClient.Do(r)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK && res.StatusCode != http.StatusNoContent && res.StatusCode != http.StatusCreated {
+		payload, _ := io.ReadAll(res.Body)
+		return fmt.Errorf("can't deactivate subscription. %s", string(payload))
+	}
+
+	return nil
+}
+
 func NewTwiplaSubscriptionAPI(
 	twiplaAPIClient *TwiplaAPIClient,
 ) *TwiplaSubscriptionAPI {
